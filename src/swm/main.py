797bb95e38d6567376e54706a91effe211b14cf2
Juanita Rodriguez Mancera <yujrodriguezm@gmail.com>
2026-05-21 11:26:47 +0200

    Add figure to visualize the data

--- src/swm/main.py
+++ src/swm/main.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+import matplotlib.pyplot as plt
 from swm.io import load_geodata
 from swm.boundingbox import get_bounding_box
 
@@ -15,11 +16,18 @@ logging.basicConfig(
 def main():
     logger.info("SWM Explorer. Starting Execution")
     
-    upl = load_geodata("upl.geojson", reproject_to=4686)
-    hei = load_geodata("ies.geojson", reproject_to=4686)
+    upl = load_geodata("upl.geojson")
+    hei = load_geodata("ies.geojson")
     
     get_bounding_box(upl)
     get_bounding_box(hei)
+    fig, ax = plt.subplots()   
+    upl.plot(ax=ax, color="lightgrey", edgecolor="white", linewidth=0.5)
+    hei.plot(ax=ax, color="steelblue", markersize=3)
+    ax.set_axis_off()
+    plt.show()
+
+    
 
 if __name__ == "__main__":
     main()
