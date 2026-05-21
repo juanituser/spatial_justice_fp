c096076903b0e9585febe052a4e894a56dba272b
Juanita Rodriguez Mancera <yujrodriguezm@gmail.com>
2026-05-21 11:27:55 +0200

    Reproject geometries to the same crs

--- src/swm/io.py
+++ src/swm/io.py
@@ -3,14 +3,14 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-def load_geodata(filename, folder="data/", reproject_to=None):
+def load_geodata(filename, folder="data/"):
     
     logger.info(f"---- loading {filename} ----")
 
     gdf = gpd.read_file(folder + filename)
     
-    if reproject_to:
-        logger.info(f"---- reprojecting to EPSG:{reproject_to} ----")
-        gdf = gdf.to_crs(epsg=reproject_to)
+    if gdf.crs != 3116:
+        logger.info(f"---- reprojecting to EPSG: 3116 ----")
+        gdf = gdf.to_crs(epsg=3116)
     
     return gdf
