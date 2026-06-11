8fa992dfc0a2b7ea4a3486b07f54e93f2ed58e23
Juanita Rodriguez Mancera <yujrodriguezm@gmail.com>
2026-06-11 10:19:31 +0200

    add functions to build the network to calculate the distance over it

--- src/swm/network.py
+++ src/swm/network.py
@@ -1,6 +1,8 @@
 import logging
+import pandana
+import pandas as pd
 import geopandas as gpd
-
+import osmnx as ox
 
 logger = logging.getLogger(__name__)
 
@@ -40,3 +42,54 @@ def get_bbox_wgs84(gdf: gpd.GeoDataFrame):
     
     return west, south, east, north
 
+def download_network(bbox, network_type):
+    """
+    Downloads the OSM street network for the bounding box.
+
+    Args:
+        bbox:         (north, south, east, west) in WGS84
+        network_type: 'walk', 'drive', or 'bike'
+
+    Returns:
+        osmnx MultiDiGraph
+    """
+    print(f"Downloading OSM {network_type} network...")
+    cf = '["highway"~"motorway|primary"]'
+    graph = ox.graph_from_bbox(
+        bbox,
+        network_type=network_type,
+        custom_filter=cf,
+        simplify=True
+    )
+    
+    print(f"Network ready — {len(graph.nodes)} nodes, {len(graph.edges)} edges")
+
+    return graph
+
+def build_pandana_network(graph) -> pandana.Network:
+    """
+    Converts an osmnx graph to a pandana Network.
+    Edge weights are lengths in meters as computed by osmnx.
+
+    Args:
+        graph: osmnx MultiDiGraph
+
+    Returns:
+        pandana.Network ready for accessibility queries
+    """
+    nodes, edges = ox.graph_to_gdfs(graph, nodes=True, edges=True)
+
+    edge_from = pd.Series(edges.index.get_level_values("u").values)
+    edge_to = pd.Series(edges.index.get_level_values("v").values)
+    edge_weights = pd.DataFrame({"distance": edges["length"].values}) #### here i can put any weight that i want
+
+    network = pandana.Network(
+        node_x=nodes["x"],
+        node_y=nodes["y"],
+        edge_from=edge_from,
+        edge_to=edge_to,
+        edge_weights=edge_weights,
+        twoway=False
+    )
+    logging.info("Pandana network ready — %s nodes", len(nodes))
+    return network
