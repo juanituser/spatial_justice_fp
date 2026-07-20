85581886b73d3fa2027931c05104154fe21d4236
Juanita Rodriguez Mancera <yujrodriguezm@gmail.com>
2026-07-20 13:10:04 +0200

    update main flow until raw accessibility is calculated

--- src/analysis/main.py
+++ src/analysis/main.py
@@ -2,13 +2,11 @@ import logging
 import sys
 import typer
 import time
-import sys
 from analysis.io import load_geodata, load_weighting_config
 from analysis.network import get_centroids, get_bbox_wgs84, download_network, build_pandana_network
 from analysis.pois import register_pois
 from analysis.accessibility import compute_accessibility
-from analysis.socioeconomic import weight_accessibility, normalize_variables
-
+from analysis.socioeconomic import weight_accessibility
 
 logger = logging.getLogger(__name__)
 
@@ -23,19 +21,19 @@ app = typer.Typer()
 @app.command()
 def main(
     polygons_file: str = typer.Option(
-        "data.geojson",
+        "munster_dummy.geojson",
         "--polygons",
         "-pl",
         help="GeoJSON filename for the polygons (e.g. District or neighbors boundaries).",
     ),
     config_file: str = typer.Option(
-        "config.csv",
+        "config_munster.csv",
         "--config",
         "-c",
         help="CSV filename with socioeconomic variables that want to be included in the analysis and its weights (e.g. District or neighbors boundaries).",
     ),
     points_file: str = typer.Option(
-        "ies.geojson",
+        "ies_munster.geojson",
         "--points",
         "-pt",
         help="GeoJSON filename for the points (e.g. Higher Education Institutions).",
@@ -47,7 +45,7 @@ def main(
         help="From OSMmnx, the options available are: all, all_public, bike, drive, drive_service, walk.",
     ),
     max_distance: float = typer.Option(
-        20000,
+        100000,
         "--max_distance",
         "-md",
         help="The maximum distance that will be used to find all the nearest pois"
@@ -73,14 +71,15 @@ def main(
 
     polygons = load_geodata(polygons_file)
     logger.info(f"Loaded: {polygons.shape[0]} features")
-    pois = load_geodata(points_file, reproject_to=4326) 
+    pois = load_geodata(points_file, reproject_to=4326) ### IN THIS CASE, HEI
     logger.info(f"Loaded: {pois.shape[0]} features")
 
     all_vars = load_weighting_config(config_file=config_file)
+    logger.info(f"Loaded {len(all_vars)} weighting variables from {config_file}")
 
-    normalize_variables(polygons, all_vars)
+    for var, weight in all_vars.items():
+        logger.info(f"  - {var}:  weight = {weight}")
 
-    sys.exit(0)
     # -------------------------------------------------------------------------- 
     # --- Get the distance from the centroids to the nearest 10 institutions ---
     # -------------------------------------------------------------------------- 
@@ -88,7 +87,6 @@ def main(
     # --- Get the centroids of the polygons ---
     centroids = get_centroids(polygons)
     logger.info(f"Calculated: {centroids.shape[0]} centroids")
-
     # --- Get the bbox of the polygons ---
     bbox = get_bbox_wgs84(polygons)
     logger.info(f"Bounding box is {bbox}")
@@ -96,15 +94,20 @@ def main(
     graph   = download_network(bbox, network_type=network_type)
     # --- Building the network using the graph ---
     network = build_pandana_network(graph)
+
     # --- Register pois in the network ---
     register_pois(network, pois, max_distance=max_distance, max_items=max_items)
-    # print(network.nodes_df.head())
 
-    raw_accessibility = compute_accessibility(network, max_distance=max_distance, num_pois=num_pois)
-    raw_accessibility.head()
+    # --- Calculate accessibility from each polygon to each POI --- 
+    raw_accessibility = compute_accessibility(network, centroids, max_distance=max_distance, num_pois=num_pois)
+    print(raw_accessibility)
+    sys.exit()
+    # ---  --- 
+    weighted_accessibility = weight_accessibility(raw_accessibility, polygons, all_vars)
+    
+    print(weighted_accessibility)
 
-    weighted_accessibility = weight_accessibility(polygons, all_vars)
-
+    # --- Visualization --- 
 
     end = time.time()
     logger.info(f"Execution time: {end - start:.2f} seconds")
